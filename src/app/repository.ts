e08d8f1655464e6e145462487863ed937d23d63a
Your Name <mwaurarita2019@gmail.com>
2019-08-26 12:03:17 +0300

    repository ts

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -2,4 +2,5 @@ export class Repository {
     constructor(public id: number,public name: string, public user: string,public link: string,public ownerPicture: string,public score: number,public language: string,public dateCreated: Date ){
 
     }
+    
 }
